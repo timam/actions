a5f4836a4f55b8be8b380b3e3ad39a621185708b
Timam <mail@timam.io>
2023-10-07 22:24:33 +0600

    re test

--- url-file-syncer/app/main.go
+++ url-file-syncer/app/main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
 	remoteFileUrl := os.Args[1]
 	localFilePath := os.Args[2]
 
-	downloadedFileName, err := downloadFile(remoteFileUrl)
-	if err != nil {
-		fmt.Printf("Error while doenloading file %s", err)
-	}
-
-	files, err := compareFiles(downloadedFileName, localFilePath)
-	if err != nil {
-		return
-	}
-
-	//message := "will compare " + remoteFileUrl + " " + localFilePath
-	message := "result of compare is " + strconv.FormatBool(files)
+	message := "will compare " + remoteFileUrl + localFilePath
 	setOutput("message", message)
 }
